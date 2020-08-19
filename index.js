4ca05a1795364b59763d5cc82919f02419452563
sourav-roy-prescriber-360 <sourav.roy@p360.com>
2020-08-19 14:54:10 -0400

    Modules added and published

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const colors=require('./colors/index');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(colors.getRandomColor());
+  // res.send('<div><h1 style="color: $colors.getRandomColor();">Hello World!</h1><p>Added a little style!</p></div>');
+  // res.send(colors.getRandomColor());
+  // res.set('text/html').send('<body bgcolor=$colors.getRandomColor()></body>');
+  res.send('<body bgcolor="$colors.getRandomColor();"></body>');
 })
 
 app.listen(port)
+
