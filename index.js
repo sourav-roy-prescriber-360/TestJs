ce78ef2a1179fd85adfd0329362750f0dde8a7b2
sourav-roy-prescriber-360 <sourav.roy@p360.com>
2020-08-19 15:20:29 -0400

    deleted color folder

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const colors=require('./colors/index');
-app.get('/', (req, res) => {
-  console.log(colors.getRandomColor());
-  // res.send('<div><h1 style="color: $colors.getRandomColor();">Hello World!</h1><p>Added a little style!</p></div>');
-  // res.send(colors.getRandomColor());
-  // res.set('text/html').send('<body bgcolor=$colors.getRandomColor()></body>');
-  res.send('<body bgcolor="$colors.getRandomColor();"></body>');
-})
+class color{
+  constructor(name,hexcode){
+      this.name=name;
+      this.hexcode=hexcode;
+  }
+}
 
-app.listen(port)
+const allColors=[
+  new color('red','#ff0000'),
+  new color('yellow','#00ffff'),
+  new color('blue','#0000ff'),
+  new color('greeen','#00ff00'),
+  new color('brown','#ffff00'),
+];
+// console.log(allColors);
+exports.getRandomColor=()=>{
+  console.log(allColors);
+  return allColors[Math.floor(Math.random()*allColors.length)].hexcode;
+}
 
+exports.allColors=allColors;
